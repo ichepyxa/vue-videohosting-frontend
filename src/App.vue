a85410b9f408b1556ed37c21629110e4917481ef
Максим Сарамудов <maxim.krytizna@gmail.com>
2022-11-13 01:32:02 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    Home page
+  </main>
+</template>
